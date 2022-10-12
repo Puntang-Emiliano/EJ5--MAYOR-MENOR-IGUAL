38020bdba6ff8db0429b6a13f16f71ffc9e4e794
Puntang Emiliano <Puntang.emiliano@gmail.com>
2022-10-11 21:46:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EJ5_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Holaaa!! Te voy a decir cual de los numeros ingresados es el MAYOR o si son IGUALES");
+
+        int nume1, nume2; 
+        Console.WriteLine("Ingrese un numero");
+        nume1= Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese el segundo numero");
+        nume2= Convert.ToInt32 (Console.ReadLine());
+        if (nume1 > nume2)
+        {
+            Console.WriteLine ("El Primer Numero es Mayor ");
+        }
+        else
+            { 
+                Console.WriteLine ("El Segundo Numero es el Mayor");
+            } 
+
+
+
+        }
+    }
+}
